651722fca6db33b1f354cc6a57c38b9c700c79f1
Hayley Christianson <hayleychristianson@gmail.com>
2017-12-08 16:47:38 -0600

    Final Updates to Warp Form
    Edited with Staff comments to the content of the message
    
    and replaced the core.log with the Discord message api so we can send messages to the Warp Proposal channel

--- src/net/kineticraft/lostcity/commands/player/CommandProposeWarp.java
+++ src/net/kineticraft/lostcity/commands/player/CommandProposeWarp.java
@@ -1,12 +1,13 @@
 package net.kineticraft.lostcity.commands.player;
 
 import net.kineticraft.lostcity.Core;
+import net.kineticraft.lostcity.EnumRank;
 import net.kineticraft.lostcity.commands.PlayerCommand;
 
 import net.kineticraft.lostcity.discord.DiscordAPI;
 import net.kineticraft.lostcity.discord.DiscordChannel;
 import net.kineticraft.lostcity.mechanics.Callbacks;
-import net.kineticraft.lostcity.utils.TextBuilder;
+import net.kineticraft.lostcity.mechanics.Chat;
 import net.kineticraft.lostcity.utils.Utils;
 
 import org.bukkit.ChatColor;
@@ -27,7 +28,7 @@ public class CommandProposeWarp extends PlayerCommand {
      */
 
     public CommandProposeWarp() {
-        super("[command]", "Submit a warp permit!","warpform");
+        super( EnumRank.GAMMA, "[command]", "Submit a warp permit!","warpform");
     }
     //initializing variables
     private static List<String> types = Arrays.asList("event", "shop", "showcase", "town", "other");
@@ -46,47 +47,50 @@ public class CommandProposeWarp extends PlayerCommand {
             "Your submission has been processed, big brother is watching!",
             "Thanks for your submission, have a happy happy joy-joy day.",
             "Your Pokemon are now healed, we hope to see you again soon!",
-            "Compiling tattle...transfluxing signal...submission sent! Thanks!",
+            "Compiling submission...transfluxing signal...submission sent! Thanks!",
             "Submission sent, don't forget to give a cookie to your Mod.",
             "Thanks for looking out homie, we got your submission!"
     );
+
     private static List<String> declineMessages  = Arrays.asList(
             "Thanks for looking, have a pleasant day!",
             "Have a great day!",
             "Let us know what you think!",
             "Thanks again, Now go find some candy"
     );
+
     private static String prefix = ChatColor.DARK_RED.toString() + ChatColor.BOLD + "Borkley" + ChatColor.GREEN + " "
             + "Warp Permit" + ChatColor.GRAY + ": " + ChatColor.WHITE;
-
-    private static String accMessage = ChatColor.GREEN + "Accept";
-
-    private static String declineMessage = ChatColor.DARK_RED + "Decline";
 
     @Override
     protected void onCommand(CommandSender sender, String[] args) {
         // you can get more properties by casting as player
         Player player = (Player) sender;
-       // boolean warpContinue = true;
-        //boolean readRules = true;
+
         sender.sendMessage(prefix + "This is to apply for a warp permit. Do you wish to continue? ");
+
         //This command is to give a prompt to confirm or cancel this request (if confirm continue, if cancel it sends back the usage to the user?)
         Callbacks.promptConfirm(player,
                 //either need to test this code into the accept function or come up with a way to store a variable to pass through here
-                () -> { sender.sendMessage(prefix + "Please stand where you would like your ideal warp before continuing.");
+                () -> { sender.sendMessage(  ChatColor.GOLD +" Please stand where you would like your ideal warp before continuing.");
                     //if accepting, continue warpform
-                    sender.sendMessage(prefix + " \n Warp Requirements: \n" +
-                            "1) A safe spawn-in and area (well-lit, no mobs, no player harm) \n" +
-                            "2) Warps must be fully claimed. \n" +
-                            "3) An active owner \n" +
-                            "4) A reapplication process each quarter to prevent dead warps. \n");
+                    sender.sendMessage(prefix +  ChatColor.BOLD + " \n Warp Requirements: \n" +
+                            ChatColor.AQUA + "1) A safe spawn-in and warp area (well-lit, no mobs, no player harm). \n" +
+                            ChatColor.DARK_AQUA + "2) Warps must be fully claimed. \n" +
+                            ChatColor.AQUA + "3) You need to apply as a warp owner and need be an active player. \n" +
+                            ChatColor.DARK_AQUA + "4) Warp Owners will reapply for warp permits each quarter to prevent dead warps. A quarter takes place Jan-March, April-June, July-Sept, Oct-Dec. \n" + ChatColor.RESET);
 
-                    sender.sendMessage(prefix + "Do you accept all requirements for the warps? ");
+                    sender.sendMessage(prefix + ChatColor.GOLD + "Do you accept all requirements for the warps? " + ChatColor.RESET);
 
                     Callbacks.promptConfirm(player,
                             () -> { sender.sendMessage(prefix +
-                                    Utils.randElement(greetingMessages) + player.getDisplayName() +
-                                    " which type of warp permit are you applying for? \n ( Event |  Shop | Showcase | Town | Exit ) " );
+                                    Utils.randElement(greetingMessages) + player.getDisplayName() + ChatColor.RESET +
+                                    ", which type of warp permit are you applying for? \n " +
+                                    "( " + ChatColor.LIGHT_PURPLE + "Event" + ChatColor.RESET + " | " +
+                                    ChatColor.DARK_GREEN + "Shop" + ChatColor.RESET + " | " +
+                                    ChatColor.GOLD + "Showcase" + ChatColor.RESET + " | " +
+                                    ChatColor.RED + "Town" + ChatColor.RESET + " | " +
+                                    ChatColor.DARK_AQUA +"Exit" + ChatColor.RESET + " ) \n Click Next to go to next option. " );
                                     //If Accepting, follow this line of code
                                     Callbacks.promptConfirm(player,
                                         () -> formWarp(sender, "event"),
@@ -101,41 +105,36 @@ public class CommandProposeWarp extends PlayerCommand {
                                                                                 () -> formWarp(sender, "town"),
                                                                                 () -> Callbacks.promptConfirm(player,
                                                                                         () -> formWarp(sender, "other"),
-                                                                                        () -> { sender.sendMessage(prefix + Utils.randElement(declineMessages)); return; },
+                                                                                        () -> { sender.sendMessage(prefix + Utils.randElement(declineMessages)); },
                                                                                         "Other Ideas!",
-                                                                                        "Exit Submission"
+                                                                                         "Exit Submission"
                                                                                 ),
-                                                                                "Town",
+                                                                                 "Town",
                                                                                 "Other"
                                                                         ),
-                                                                        "Showcase",
+                                                                         "Showcase",
                                                                         "Next"
                                                                 )
                                                         ,
-                                                        "Shop",
+                                                         "Shop",
                                                         "Next"
                                                 )
                                         ,
-                                        "Event",
+                                              "Event",
                                         "Next"
                                     );
 
                                     },
                             () -> { sender.sendMessage(prefix + Utils.randElement(declineMessages)); },
-                            accMessage,
-                            declineMessage
+                            ChatColor.GREEN.BOLD + "Accept",
+                            ChatColor.DARK_RED.BOLD + "Decline"
                             );
                 },
 
                 () -> { sender.sendMessage(prefix + Utils.randElement(declineMessages)); },
-                accMessage,
-                declineMessage
+                ChatColor.GREEN.BOLD + "Accept",
+                ChatColor.DARK_RED.BOLD + "Decline"
                 );
-        // (This was used before I got promptConfirm working player.sendMessage(textBuilder.create());
-
-       // else{
-       //     sender.sendMessage(prefix + Utils.randElement(declineMessages));
-        //}
 
     }
 
@@ -144,26 +143,53 @@ public class CommandProposeWarp extends PlayerCommand {
         if (types.contains(type)) {
 
             if (type.equals("event")) {
-                sender.sendMessage(prefix + "Why do you think your event should become a warp?");
+                sender.sendMessage(prefix + " Event Requirements: \n" +
+                                    ChatColor.AQUA + "1) Event is publicly posted in #Get-Togethers channel on Discord. \n" +
+                                    ChatColor.DARK_AQUA + "2) If temporary, please state the dates needed for the warp. \n"
+                        +
+                        prefix + "Describe your Event: ");
                 Callbacks.listenForChat(player, description -> submitWarp(player, type, description));
 
             } else if (type.equals("shop")) {
-                sender.sendMessage(prefix + "Why do you think your shop should become a warp?");
+                sender.sendMessage(prefix + " Shop Requirements: \n" +
+                                    ChatColor.AQUA +"1) A Rules and information Board. \n" +
+                                    ChatColor.DARK_AQUA +"2) At least 8 active players who own a plot/build and keep items stocked. If the build is a solo shop, it must fit into a Showcase quality standard. \n" +
+                                    ChatColor.AQUA +"3) A selection of empty and ready plots available. \n" +
+                                    ChatColor.DARK_AQUA +"4) If you are inactive, name a user who will take over your duties to prevent the warp from being removed. \n"
+                        +
+                prefix + "Describe your Shop: ");
                 Callbacks.listenForChat(player, description -> submitWarp(player, type, description));
 
             } else if (type.equals("showcase")) {
-                sender.sendMessage(prefix + "Why do you think your showcase should become a warp?");
+                sender.sendMessage(prefix + " Showcase Requirements: \n" +
+                                    ChatColor.AQUA + "1) An Info Board at warp-in. \n" +
+                                    ChatColor.DARK_AQUA + "2) A finished/completed build. \n" +
+                                    ChatColor.AQUA + "3) Build shows excellent build quality and standards. \n" +
+                                    ChatColor.DARK_AQUA + "4) Build is fully claimed. \n"
+                        +
+                prefix + "Describe your Showcase: ");
                 Callbacks.listenForChat(player, description -> submitWarp(player, type, description));
 
             } else if (type.equals("town")) {
-                sender.sendMessage(prefix + "Why do you think your town should become a warp?");
+                sender.sendMessage(prefix + " Town Requirements: \n" +
+                                    ChatColor.AQUA + "1) A Rules and information Board. \n" +
+                                    ChatColor.DARK_AQUA + "2) At least 6 active players who own a plot/build and keep items stocked. \n" +
+                                    ChatColor.AQUA + "3) A selection of empty and ready plots available. \n" +
+                                    ChatColor.DARK_AQUA + "4) If you are inactive, name a user who will take over your duties to prevent the warp from being removed. \n"
+                        +
+                prefix + "Describe your Town: ");
                 Callbacks.listenForChat(player, description -> submitWarp(player, type, description));
 
             }
             else if (type.equals("other")) {
-                sender.sendMessage(prefix + "\"Please describe your own warp idea");
-                Callbacks.listenForChat(player, description -> submitWarp(player, type, description));
-
+                sender.sendMessage(prefix + " Other Warp Requirements: \n" +
+                                    ChatColor.AQUA + "1) A Rules and information Board. \n" +
+                                    ChatColor.DARK_AQUA + "2) Build is fully claimed. \n" +
+                                    ChatColor.AQUA + "3) If you are inactive, name a user who will take over your duties to prevent the warp from being removed. \n"
+                        +
+                prefix + "For Other Warp Options: Please place suggestions in the Suggestions Channel on our discord.");
+                //Callbacks.listenForChat(player, description -> submitWarp(player, type, description));
+                sender.sendMessage(prefix + Utils.randElement(declineMessages));
             }
 
         }
@@ -179,9 +205,8 @@ public class CommandProposeWarp extends PlayerCommand {
                 "```\n" +
                 description + "\n" +
                 "```";
-        //DiscordAPI.sendMessage(DiscordChannel.WARP_PROPOSALS, message);
-        //Chat.log something need to ask Sab
-        Core.logInfo(message);
+        DiscordAPI.sendMessage(DiscordChannel.WARP_PROPOSALS, message);
+        //Core.logInfo(message);
         reporter.sendMessage(prefix + Utils.randElement(exitMessages));
     }
     private static void endMessage(Player reporter){
